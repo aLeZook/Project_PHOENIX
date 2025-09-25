68e9e3891acc0c5ac668467c807975f06fa961aa
Alexis <4lexis1212@gmail.com>
2025-09-25 12:26:57 -0700

    Initial Commit. Added a bottom navigation menu with functioning fragments. When you click the bottom menu, it takes you to the different fragments.

---
+++ app/src/main/java/com/example/project_phoenix/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.project_phoenix
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        bottomNavigationView = findViewById(R.id.bottom_nav)
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+
+            when (menuItem.itemId) {
+                R.id.bottom_home -> replaceFragment(HomeFragment())
+                R.id.bottom_settings -> replaceFragment(SettingsFragment())
+                R.id.bottom_add -> replaceFragment(HomeFragment())
+                R.id.bottom_calendar -> replaceFragment(CalendarFragment())
+                R.id.bottom_challenges -> replaceFragment(ChallengesFragment())
+                R.id.bottom_stats -> replaceFragment(StatsFragment())
+
+            }
+            true
+        }
+
+    }
+    private fun replaceFragment(fragment: Fragment)
+    {
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container,fragment).commit()
+    }
+}
